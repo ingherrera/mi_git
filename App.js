6d99bbfb22bdee18083f11f80f99283fa7f23a3c
ingherrera <ing_herrera16@hotmail.com>
2020-06-12 13:13:28 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+
+
+function reducer(state, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+const initialState={
+  countryList:[]
+}
+
+const store = createStore(reducer, initialState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+function App(){
+  return (
+    <Provider store={store}>
+      <main>
+        todo ok
+      </main>
+    </Provider>
+
+    
+  )
+};
+
+export default App;
